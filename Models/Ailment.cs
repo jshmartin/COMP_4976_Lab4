3d614ad6ab5da8130fb1a332b56743bc810f5cff
ChillyChaos <josh123198@gmail.com>
2022-10-05 23:23:41 -0700

    first commit

---
+++ Models/Ailment.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Ailment
+{
+[Key]
+public string? Name { get; set; }
+[ForeignKey("PatientId")]
+public Patient? Patient { get; set; }
+public int PatientId { get; set; }
+}
